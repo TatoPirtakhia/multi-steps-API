96c74e7af5807946758f5d27c1e9428bb268c113
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-05-17 11:57:31 +0400

    fix controlerr

--- src/controllers/constrollers.ts
+++ src/controllers/constrollers.ts
@@ -3,6 +3,7 @@ import MultiSteps from "../model/multiSteps.js";
 import postNewData from "../Schema/validatorJoi.js";
 
 export const addNewSteps = async (req: Request, res: Response) => {
+  console.log("helloo")
   const { body } = req;
   const validator = await postNewData();
   const { value, error } = validator.validate(body);
@@ -12,7 +13,7 @@ export const addNewSteps = async (req: Request, res: Response) => {
     return res.status(422).json(error.details);
   }
   const { name, email ,phone ,plan,addOns} = value;
-
+  console.log('is not error')
   const last = await MultiSteps.find().sort({ _id: -1 }).limit(1);
   let id = last.length > 0 ? last[0].id + 1 : 1;
   if (isNaN(id)) {
@@ -27,7 +28,7 @@ export const addNewSteps = async (req: Request, res: Response) => {
     addOns
 
   };
-
+  console.log('send data')
   await MultiSteps.create({ ...data });
 
   return res.status(201).json({ ...data });
