4d890a20e9a3fb8b68e87d396f2f9348914a2eba
TatoPirtakhia <tatopirtakhia@gmail.com>
2023-05-17 12:14:26 +0400

    remove consoles

--- src/controllers/constrollers.ts
+++ src/controllers/constrollers.ts
@@ -3,7 +3,6 @@ import MultiSteps from "../model/multiSteps.js";
 import postNewData from "../Schema/validatorJoi.js";
 
 export const addNewSteps = async (req: Request, res: Response) => {
-  console.log("helloo")
   const { body } = req;
   const validator = await postNewData();
   const { value, error } = validator.validate(body);
@@ -12,8 +11,7 @@ export const addNewSteps = async (req: Request, res: Response) => {
     console.log("heloo");
     return res.status(422).json(error.details);
   }
-  const { name, email ,phone ,plan,addOns} = value;
-  console.log('is not error')
+  const { name, email, phone, plan, addOns } = value;
   const last = await MultiSteps.find().sort({ _id: -1 }).limit(1);
   let id = last.length > 0 ? last[0].id + 1 : 1;
   if (isNaN(id)) {
@@ -25,10 +23,8 @@ export const addNewSteps = async (req: Request, res: Response) => {
     email,
     phone,
     plan,
-    addOns
-
+    addOns,
   };
-  console.log('send data')
   await MultiSteps.create({ ...data });
 
   return res.status(201).json({ ...data });
